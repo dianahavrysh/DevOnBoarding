578a36fb8f4ea7b8b691479b2ea3430b480d7f6b
dianahavrysh <dhavrysh1205@gmail.com>
2026-08-27 15:52:27 +0300

    fix: add output param

--- DataLayer/SQLScripts/MSSQL/Users_INS.sql
+++ DataLayer/SQLScripts/MSSQL/Users_INS.sql
@@ -1,62 +1,31 @@
-ALTER PROCEDURE dbo.Users_INS
-    @UserName NVARCHAR(50),
-    @Email NVARCHAR(50),
-    @Password NVARCHAR(50),
-    @ActiveStatus BIT,
-    @RoleTypePK UNIQUEIDENTIFIER,
-    @FirstName NVARCHAR(50),
-    @SecondName NVARCHAR(50),
-    @BirthDate DATETIME2(7)
+CREATE PROCEDURE dbo.Users_INS
+	@UserName NVARCHAR(50),
+	@Email NVARCHAR(50),
+	@Password NVARCHAR(50),
+	@ActiveStatus BIT,
+	@RoleTypePK UNIQUEIDENTIFIER,
+	@FirstName NVARCHAR(50),
+	@SecondName NVARCHAR(50) = NULL,
+	@BirthDate DATETIME2(7) = NULL,
+	@NewUserPK UNIQUEIDENTIFIER OUTPUT
 AS
 BEGIN
-    SET NOCOUNT ON;
-    BEGIN TRANSACTION;
-    BEGIN TRY
-        DECLARE @UserPK UNIQUEIDENTIFIER = NEWID();
+	SET NOCOUNT ON;
+	BEGIN TRANSACTION;
+	BEGIN TRY
+		SET @NewUserPK = NEWSEQUENTIALID();
 
-        INSERT INTO dbo.Users
-        (
-            UserPK,
-            UserName,
-            Email,
-            Password,
-            ActiveStatus,
-            RoleTypePK
-        )
-        VALUES
-        (
-            @UserPK,
-            @UserName,
-            @Email,
-            @Password,
-            @ActiveStatus,
-            @RoleTypePK
-        );
+		INSERT INTO dbo.Users (UserPK, UserName, Email, Password, ActiveStatus, RoleTypePK)
+		VALUES (@NewUserPK, @UserName, @Email, @Password, @ActiveStatus, @RoleTypePK);
 
-        INSERT INTO dbo.UserData
-        (
-            UserPK,
-            FirstName,
-            SecondName,
-            BirthDate
-        )
-        VALUES
-        (
-            @UserPK,
-            @FirstName,
-            @SecondName,
-            @BirthDate
-        );
+		INSERT INTO dbo.UserData (UserPK, FirstName, SecondName, BirthDate)
+		VALUES (@NewUserPK, @FirstName, @SecondName, @BirthDate);
 
-        COMMIT TRANSACTION;
-
-        SELECT @UserPK AS NewUserPK;
-    END TRY
-    BEGIN CATCH
-        IF @@TRANCOUNT > 0
-            ROLLBACK TRANSACTION;
-
-        THROW;
-    END CATCH
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+		IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
+		THROW;
+	END CATCH
 END;
 GO
