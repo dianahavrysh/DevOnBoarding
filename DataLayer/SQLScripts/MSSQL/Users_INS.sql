fce2fb52da1966e380a6381e2bc118e60a899be5
dianahavrysh <dhavrysh1205@gmail.com>
2026-08-27 13:39:48 +0300

    refactor: simplify user insert procedure

--- DataLayer/SQLScripts/MSSQL/Users_INS.sql
+++ DataLayer/SQLScripts/MSSQL/Users_INS.sql
@@ -1,65 +1,62 @@
 ALTER PROCEDURE dbo.Users_INS
-	@UserName NVARCHAR(50),
-	@Email NVARCHAR(50),
-	@Password NVARCHAR(50),
-	@ActiveStatus BIT,
-	@RoleTypePK UNIQUEIDENTIFIER,
-	@FirstName NVARCHAR(50),
-	@SecondName NVARCHAR(50),
-	@BirthDate DATETIME2(7)
+    @UserName NVARCHAR(50),
+    @Email NVARCHAR(50),
+    @Password NVARCHAR(50),
+    @ActiveStatus BIT,
+    @RoleTypePK UNIQUEIDENTIFIER,
+    @FirstName NVARCHAR(50),
+    @SecondName NVARCHAR(50),
+    @BirthDate DATETIME2(7)
 AS
 BEGIN
-	SET NOCOUNT ON;
-	BEGIN TRANSACTION;
-	BEGIN TRY
-		DECLARE @InsertedIds TABLE (UserPK UNIQUEIDENTIFIER);
-		DECLARE @NewUserPK UNIQUEIDENTIFIER;
+    SET NOCOUNT ON;
+    BEGIN TRANSACTION;
+    BEGIN TRY
+        DECLARE @UserPK UNIQUEIDENTIFIER = NEWID();
 
-		INSERT INTO dbo.Users
-		(
-			UserName,
-			Email,
-			Password,
-			ActiveStatus,
-			RoleTypePK
-		)
-		OUTPUT INSERTED.UserPK INTO @InsertedIds
-		VALUES
-		(
-			@UserName,
-			@Email,
-			@Password,
-			@ActiveStatus,
-			@RoleTypePK
-		);
+        INSERT INTO dbo.Users
+        (
+            UserPK,
+            UserName,
+            Email,
+            Password,
+            ActiveStatus,
+            RoleTypePK
+        )
+        VALUES
+        (
+            @UserPK,
+            @UserName,
+            @Email,
+            @Password,
+            @ActiveStatus,
+            @RoleTypePK
+        );
 
-		SELECT @NewUserPK = UserPK
-		FROM @InsertedIds;
+        INSERT INTO dbo.UserData
+        (
+            UserPK,
+            FirstName,
+            SecondName,
+            BirthDate
+        )
+        VALUES
+        (
+            @UserPK,
+            @FirstName,
+            @SecondName,
+            @BirthDate
+        );
 
-		INSERT INTO dbo.UserData
-		(
-			UserPK,
-			FirstName,
-			SecondName,
-			BirthDate
-		)
-		VALUES
-		(
-			@NewUserPK,
-			@FirstName,
-			@SecondName,
-			@BirthDate
-		);
+        COMMIT TRANSACTION;
 
-		COMMIT TRANSACTION;
+        SELECT @UserPK AS NewUserPK;
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
 
-		SELECT @NewUserPK AS NewUserPK;
-	END TRY
-	BEGIN CATCH
-		IF @@TRANCOUNT > 0
-			ROLLBACK TRANSACTION;
-
-		THROW;
-	END CATCH
+        THROW;
+    END CATCH
 END;
 GO
